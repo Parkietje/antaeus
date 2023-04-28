ae4cd6071dc71910ac9d5f1d84a6d14b79bff7a7
Parkietje <yannichiodi@gmail.com>
2023-04-28 15:00:10 +0200

    implement billingservice and add a test for it

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,28 @@
 package io.pleo.antaeus.core.services
 
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.locks.ReentrantLock
 
-class BillingService(
-    private val paymentProvider: PaymentProvider
-) {
-// TODO - Add code e.g. here
+
+class BillingService(private val paymentProvider: PaymentProvider) {
+
+    private val invoiceLocks = ConcurrentHashMap<Int, ReentrantLock>()
+
+    fun process(invoice: Invoice) {
+        val lock = invoiceLocks.computeIfAbsent(invoice.id) { ReentrantLock() }
+        try {
+            lock.lock()
+            if (invoice.status != InvoiceStatus.PAID) {
+                val success = paymentProvider.charge(invoice)
+                if (success) {
+                    // Update the invoice in the database
+                }
+            }
+        } finally {
+            lock.unlock()
+        }
+    }
 }
---
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
@@ -0,0 +1,45 @@
+package io.pleo.antaeus.core.services
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+
+import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.Currency
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
+import io.pleo.antaeus.models.Money
+
+import org.junit.jupiter.api.Test
+import java.math.BigDecimal
+import kotlin.concurrent.thread
+
+
+class BillingServiceTest {
+
+    // test invoice
+    private val money = Money(BigDecimal.ONE,Currency.DKK)
+    private var invoice = Invoice(1,1,money,InvoiceStatus.PENDING)
+
+    // mock provider and add some processing delay
+    private val provider = mockk<PaymentProvider> {
+        every { charge(invoice) } answers {
+            Thread.sleep(1000)
+            true
+        }
+    }
+
+    private val billingService = BillingService(paymentProvider = provider)
+
+    @Test
+    fun `will assert charge is only called once when using multiple threads`() {
+        thread {
+            billingService.process(invoice)
+        }
+        thread {
+            billingService.process(invoice)
+        }
+
+        verify(exactly = 1) { provider.charge(invoice) }
+    }
+}
